e37e263b5c5c1131feceb3b9680349ed9468a4d4
victor-renan <alvesrenanpro@gmail.com>
2023-03-16 16:41:10 -0300

    Adicionei o footer

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -2,7 +2,16 @@ import React from "react";
 import './index.css'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
-import { Whatsapp, Instagram } from "react-bootstrap-icons";
+import logo from "../../assets/images/logo-alternativa.svg"
+
+// Icons
+import {
+  Whatsapp,
+  Instagram,
+  PeopleFill,
+  ClockFill,
+  GeoAltFill
+} from "react-bootstrap-icons";
 
 // Routes
 import { HomeRoute } from "../../pages/Home";
@@ -10,7 +19,7 @@ import { LojasRoute } from "../../pages/Lojas";
 import { EventosRoute } from "../../pages/Eventos";
 
 // Router
-import { Link } from "react-router-dom";
+import { Link, Route } from "react-router-dom";
 
 
 const Footer = () => {
@@ -18,9 +27,9 @@ const Footer = () => {
     <footer>
       <div className="social">
         <div className="social__logo">
-          <img src="" alt="Logo Ipê Center" />
+          <img src={logo} className="logo__imagem" alt="Logo Ipê Center" />
         </div>
-        <div className="social__item">
+        <div className="social__items">
           <a href="#" className="item__link">
             <Whatsapp className="link__icon" />
           </a>
@@ -41,6 +50,26 @@ const Footer = () => {
           Eventos
         </Link>
       </Nav>
+      <hr className="footer__separator" />
+      <div className="footer__info">
+        <p className="info__meta">&copy;023 - Shopping Ipê Center. Todos os direitos reservados.</p>
+        <div className="info__data">
+          <span className="data__trabalho">
+            <PeopleFill className="data__icon" />
+            <Link to={HomeRoute}>Trabalhe conosco</Link>
+          </span>
+          <span className="data__horarios">
+            <span>
+              <ClockFill className="data__icon" />
+              <p className="data__title">Horários de funcionamento</p>
+            </span>
+            <ul className="data__list">
+              <li className="list__item"></li>
+              <li className="list__item"></li>
+            </ul>
+          </span>
+        </div>
+      </div>
     </footer>
   );
 };
