5523bd6e33610f6c5024b0983f0a10eb6af2e0f8
Aparecido Silva <aparecido.silva12y@gmail.com>
2023-03-18 16:15:21 -0300

    Coloquei o borde-bottom no - veja loja

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -74,7 +74,7 @@ const Footer = () => {
           </div>
         </div>
         <div className="data__maps">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d989.5623520380977!2d-40.13716997079871!3d-7.21236426918099!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x79f5ace6a6c6549%3A0xdb7a397a650e8f34!2sR.%20Santo%20Ant%C3%B4nio%2C%20346%20-%20Araripe%2C%20CE%2C%2063170-000!5e0!3m2!1spt-BR!2sbr!4v1679164487566!5m2!1spt-BR!2sbr" width="100%" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          
         </div>
       </div>
     </footer>
