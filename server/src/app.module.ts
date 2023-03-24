af51c95d013a8b6f89c3d29444083210393651ca
Renan Alves <alvesrenanpro@gmail.com>
2023-03-24 14:38:40 -0300

    chore(): app module modified.

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './api/user/user.module';
 import { ApiModule } from './api/api.module';
+import { TypeOrmConfigService } from './shared/typeorm/typeorm.service';
+import { getEnviromentPath } from './common/helper/env.helper';
+
+const envFilePath: string = getEnviromentPath(`${__dirname}/common/envs`);
+
 
 @Module({
-  imports: [UserModule, ApiModule],
+  imports: [
+    ConfigModule.forRoot({ envFilePath, isGlobal: true }),
+    TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService }),
+    ApiModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
