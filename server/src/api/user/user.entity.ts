e655e03a8d4e42796fe5e2ccd52e24aebf59e17d
victor-renan <alvesrenanpro@gmail.com>
2023-03-27 23:19:53 -0300

    main.ts and auth.helper.ts modified with business logic

---
+++ server/src/api/user/user.entity.ts
@@ -0,0 +1,26 @@
+import { Exclude } from "class-transformer";
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn
+} from "typeorm";
+
+@Entity()
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  public id!: number;
+
+  @Column({ type: 'varchar' })
+  public name: string;
+
+  @Exclude()
+  @Column({ type: 'varchar' })
+  public password!: string;
+
+  @Column({ type: 'varchar' })
+  public email: string;
+
+  @Column({ type: 'timestamp', nullable: true, default: null })
+  public lastLoginAt: Date | null;
+}
