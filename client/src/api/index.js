c68e72762f00a4e34be71ad5df1aabb9b31bac85
victor-renan <alvesrenanpro@gmail.com>
2023-07-05 00:39:55 -0300

    feat: user services, api reformulada

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,20 +1,3 @@
-import axios from "axios";
-import { PORT } from "./util/constants";
-import { UserRoutes } from "./routes/UserRoutes";
-import { DemandRoutes } from "./routes/DemandRoutes";
-import { EventRoutes } from "./routes/EventRoutes";
-import { ComunicateRoutes } from "./routes/ComunicateRoutes";
-import { CategoryRoutes } from "./routes/CategoryRoutes";
+import { UsersService } from "./services/Users";
 
-
-export const ApiInstance = axios.create({
-  baseURL: `http://localhost:${PORT}`
-});
-
-export { 
-  UserRoutes,
-  DemandRoutes,
-  EventRoutes,
-  ComunicateRoutes,
-  CategoryRoutes
-}
+export const Users = UsersService;
