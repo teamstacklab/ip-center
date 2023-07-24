97e3cf184af3bfc0fbed9665807d4d7274528688
Aparecido <Aparecido.silva12y@gmail.com>
2023-07-24 14:32:25 -0300

    Finalizado as páginas de Demandas, login e registro

--- client/src/pages/Demands/index.jsx
+++ client/src/pages/Demands/index.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { styled } from "styled-components";
 
-import './CSS/mobile.css';
-import './CSS/desktop.css';
 import * as Icons from "react-bootstrap-icons";
 import { Title } from "@/components/Title";
 import * as Box from "@/components/Box";
@@ -15,13 +13,19 @@ const Section = styled(Box.Section)`
         gap: 1rem;
   }
 `
+const ContainerDemandas = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1em;
+  margin-top: 2em;
+`
 
 const Demandas = () => {
   return (
     <Section>
       <Title icon={<Icons.CheckAll />}>Demandas de Registro</Title>
-      <div className="demandas__cards">
-      </div>
+      <ContainerDemandas >
+      </ContainerDemandas>
     </Section>
   );
 };
--- client/src/pages/Login/index.jsx
+++ client/src/pages/Login/index.jsx
@@ -1,25 +1,83 @@
 import React from "react";
 import { LoginForm } from "../../components/Forms/Login";
 import Logo from "../../assets/images/logo.svg";
-import "./CSS/mobile.css";
-import "./CSS/desktop.css";
+import LogoAltenativa from "../../assets/images/logo-alternativa.svg"
+
+import { styled } from "styled-components";
+
+export const Main = styled.main`
+  margin-top: var(--nav-top-desktop);
+  padding: 40px 12px;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  @media screen and (max-width: 1440px) {
+    margin-top: var(--nav-top-mobile);
+    padding: 0;
+  }
+`
+
+export const Section =  styled.section`
+  display: flex;
+  flex-direction: row;
+  width: 50vw;
+  border-radius: 12px;
+  overflow: hidden;
+  background: var(--orange-tertiary);
+  @media screen and (max-width: 1440px) {
+    flex-direction: column;
+    padding: var(--padding-mobile);
+    width: 100%;
+    max-width: 480px;
+    background: transparent;
+
+  }
+`
+
+export const Div = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: ${props => props.$DivLogo ? "0" : "32px 24px"};
+  gap: 20px;
+  width: 100%;
+  background-color: ${props => props.$DivLogo ? "var(--brown-primary)" :"none"};
+  border-radius: 12px;
+  @media screen and (max-width: 1440px) {
+    display: ${props => props.$DivLogo ? "none" : "flex"};
+    padding: 0;
+  }
+`
+
+export const LogoImage = styled.img`
+  width: 120px;
+  height: fit-content;
+  object-fit: cover;
+`
+export const LogoImageAltenativa = styled.img`
+  width: 220px;
+  object-fit: cover;
+`
 
 
 export const LoginRoute = '/login';
 
 export default function Login() {
   return (
-    <main id="login">
-      <section className="auth__login">
-        <div className="login__group">
-          <img className="login__image" src={Logo} alt="Logo do Ipe" />
+    <Main>
+      <Section>
+        <Div>
+          <LogoImage src={Logo} alt="Logo do Ipe center" />
           <p>Faça <b>LogIn</b> para ter acesso a sua loja!</p>
           <LoginForm />
-        </div>
-        <div className="login__background">
-          <img className="login__background__image" src={LogoAlt} alt="Logo Ipe alternativa" />
-        </div>
-      </section>
-    </main>
+        </Div>
+        <Div $DivLogo>
+          <LogoImageAltenativa src={LogoAltenativa} alt="Logo Ipe alternativa" />
+        </Div>
+      </Section>
+    </Main>
   );
 }
