f56db4397b2cdd9c7bac347665ab80ceddd497ff
Renan <alvesrenanpro@gmail.com>
2023-08-02 18:06:18 -0300

    fix: relative imports added

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,7 +3,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET, POST',
+    credentials: true,
+  })
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
