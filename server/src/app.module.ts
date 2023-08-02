cf4808e19d592d1d1c5017c2272a1b53045f41e0
Renan <alvesrenanpro@gmail.com>
2023-08-02 15:57:17 -0300

    chore: relative imports

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
-import { DatabaseModule } from './ioc/Database.module';
-import { UserModule } from './ioc/User.module';
-import { DemandModule } from './ioc/Demand.module';
-import { StoreModule } from './ioc/Store.module';
-import { CategoryModule } from './ioc/Category.module';
-import { EventModule } from './ioc/Event.module';
-import { ComunicateModule } from './ioc/Comunicate.module';
+import { DatabaseModule } from 'ioc/Database.module';
+import { UserModule } from 'ioc/User.module';
+import { DemandModule } from 'ioc/Demand.module';
+import { StoreModule } from 'ioc/Store.module';
+import { CategoryModule } from 'ioc/Category.module';
+import { EventModule } from 'ioc/Event.module';
+import { ComunicateModule } from 'ioc/Comunicate.module';
 import { ConfigModule } from '@nestjs/config';
-import { EncryptionModule } from './ioc/Encryption.module';
-import { AuthModule } from './ioc/Auth.module';
-import { ImageModule } from './ioc/Image.module';
+import { EncryptionModule } from 'ioc/Encryption.module';
+import { AuthModule } from 'ioc/Auth.module';
+import { ImageModule } from 'ioc/Image.module';
 
 @Module({
   imports: [
